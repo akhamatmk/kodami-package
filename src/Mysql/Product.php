06fd848883398246b960b23437fd82f1f3bfc55e
akhamat cool <newbie.hao.semarang@gmail.com>
2018-01-28 21:54:02 +0700

    add models product and product images

---
+++ src/Mysql/Product.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Kodami\Models\Mysql;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Product extends Model
+{
+	use SoftDeletes;
+
+    protected $fillable = [
+        'koprasi_id', 'category_id', 'description', 'price', 'primary_image', 'avaible', 'success_transaction', 'total_comment', 'weight', 'viewer', 'stock', 'new',
+    ];
+
+    public function category()
+    {
+    	return $this->hasOne('Kodami\Models\Mysql\Category', 'id', 'category_id');
+    }
+
+    public function image()
+    {
+    	return $this->hasMany('Kodami\Models\Mysql\ProductImage', 'product_id', 'id');
+    }
+}
---
+++ src/Mysql/ProductImage.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Kodami\Models\Mysql;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class ProductImage extends Model
+{
+	use SoftDeletes;
+
+    protected $fillable = [
+        'product_id', 'image',
+    ];
+    
+}

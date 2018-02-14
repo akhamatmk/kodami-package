ff2021ad5ec165f5ce69f3ec9569fb6179c5b191
Ramdoni <doni.enginer@gmail.com>
2018-02-14 17:01:09 +0700

    Update model GroupUser

--- src/Mysql/UserGroup.php
+++ src/Mysql/UserGroup.php
@@ -1,10 +1,13 @@
 <?php
 
-namespace App;
+namespace Kodami\Models\Mysql;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class UserGroup extends Model
 {
-    //
+	use SoftDeletes;
+    
+    protected $table = 'user_group';
 }

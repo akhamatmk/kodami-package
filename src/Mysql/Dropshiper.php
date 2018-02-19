7da6da98a53e49914ded002d5f2978defa401757
akhamat cool <newbie.hao.semarang@gmail.com>
2018-02-19 19:15:53 +0700

    commit this

--- src/Mysql/Dropshiper.php
+++ src/Mysql/Dropshiper.php
@@ -10,6 +10,6 @@ class Dropshiper extends Model
 {
     protected $primaryKey = 'id';
     protected $fillable = [
-        'member_id', 'province_id', 'district_id', 'village_id', 'has_physical_store', 'occupation', 'place_to_sell', 'postal_code', 'location', 'is_active', 
+        'member_id', 'province_id', 'regency_id', 'district_id', 'village_id', 'has_physical_store', 'occupation', 'place_to_sell', 'postal_code', 'location', 'image', 'is_active', 
     ];
 }
---
+++ src/Mysql/Village.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Kodami\Models\Mysql;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Village extends Model
+{
+	use SoftDeletes;
+}

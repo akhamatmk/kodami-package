f4941cd04366d3e67c6b43d92691c73161c9355b
akhamat cool <newbie.hao.semarang@gmail.com>
2018-03-22 13:54:17 +0700

    commit this

--- src/Mysql/BannerSlideshow.php
+++ src/Mysql/BannerSlideshow.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class BannerSlideshow extends Model
 {
     protected $table = 'banner_slideshow';
+
+    public function product()
+    {
+    	return $this->hasOne('Kodami\Models\Mysql\KodamiProduct', 'id', 'kodami_product_id');
+    }
 }

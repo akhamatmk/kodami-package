b5bc17e1f89d7f2a860fc171d99c9660e3fe4987
akhamat cool <newbie.hao.semarang@gmail.com>
2018-05-29 22:35:01 +0700

    commit all change

--- src/Mysql/Koprasi.php
+++ src/Mysql/Koprasi.php
@@ -13,5 +13,15 @@ class Koprasi extends Model
     protected $fillable = [
         'member_id', 'regency_id', 'name', 'url', 'slogan', 'description', 'image', 'pickup_address', 'postal_code',
     ];
+
+    public function regency()
+    {
+    	return $this->hasOne('Kodami\Models\Mysql\Regency', 'id', 'regency_id');
+    }
+
+    public function member_2()
+    {
+    	return $this->hasOne('Kodami\Models\Mysql\Member_2', 'id', 'member_id');	
+    }
     
 }

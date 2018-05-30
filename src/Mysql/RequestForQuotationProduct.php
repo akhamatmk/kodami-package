38b9669b94343038b312832f2d72156390272cfa
akhamat cool <newbie.hao.semarang@gmail.com>
2018-05-30 09:48:30 +0700

    commit all change

---
+++ src/Mysql/RequestForQuotationProduct.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Kodami\Models\Mysql;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class RequestForQuotationProduct extends Model
+{
+	use SoftDeletes;
+	
+}

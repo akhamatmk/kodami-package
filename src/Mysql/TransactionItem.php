0f8a4e651569fe9e774e5d3758f27c12b725c54b
akhamat cool <newbie.hao.semarang@gmail.com>
2018-06-28 17:09:03 +0700

    commit all change

--- src/Mysql/TransactionItem.php
+++ src/Mysql/TransactionItem.php
@@ -7,5 +7,15 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class TransactionItem extends Model
 {
-	use SoftDeletes;	
+	use SoftDeletes;
+
+	public function district()
+    {
+    	return $this->hasOne('Kodami\Models\Mysql\District', 'id', 'district_id');
+    }
+
+    public function product()
+    {
+    	return $this->hasOne('Kodami\Models\Mysql\KodamiProduct', 'id', 'kodami_product_id');	
+    }
 }

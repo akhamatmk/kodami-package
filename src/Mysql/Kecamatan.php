beb0cfd5d208e4b3c414ba5a3b93520f45ba0db8
Ramdoni <doni.enginer@gmail.com>
2018-07-23 13:50:19 +0700

    Kodami Add Provinsi, Kabupaten, Kecamatan, Kelurahan

---
+++ src/Mysql/Kecamatan.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Kodami\Models\Mysql;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Kecamatan extends Model
+{
+    protected $table = 'kecamatan';
+}
---
+++ src/Mysql/Provinsi.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Kodami\Models\Mysql;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Provinsi extends Model
+{
+    protected $table = 'provinsi';
+}

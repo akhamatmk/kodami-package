e39b1fc13f422391e520b22916c197adb64902cb
System Administrator <root@Ramdonis-MacBook-Pro.local>
2018-09-05 11:21:00 +0700

    Mutation

--- src/Mysql/Mutation.php
+++ src/Mysql/Mutation.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Mutation extends Model
 {
+ 	
+ 	protected $table = 'mutations';
     
+    /**
+     * [bank description]
+     * @return [type] [description]
+     */
+    public function bank()
+    {
+    	return $this->hasOne('Kodami\Models\Mysql\Bank', 'id', 'bank_id');
+    }   
 }

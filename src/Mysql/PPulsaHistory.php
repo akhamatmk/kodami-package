02d25729a204c9a9823517dd26fa227fca7b3b69
Ramdoni <doni.enginer@gmail.com>
2018-10-17 22:03:28 +0700

    Update Pulsa History

--- src/Mysql/PPulsaHistory.php
+++ src/Mysql/PPulsaHistory.php
@@ -7,4 +7,22 @@ use Illuminate\Database\Eloquent\Model;
 class PPulsaHistory extends Model
 {
     protected $table = 'p_pulsa_history';
+
+    /**
+     * [user description]
+     * @return [type] [description]
+     */
+    public function user()
+    {
+    	return $this->hasOne('\Kodami\Models\Mysql\Users', 'id', 'user_id');
+    }
+
+    /**
+     * [pulsa description]
+     * @return [type] [description]
+     */
+    public function pulsa()
+    {
+    	return $this->hasOne('\Kodami\Models\Mysql\PPulsa', 'id', 'p_pulsa_id');
+    }
 }

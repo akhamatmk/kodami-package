a6f0559e9733033b79ca8446b5c0f4812cb2066a
Ramdoni <doni.enginer@gmail.com>
2018-11-01 13:36:52 +0700

    Update User dropshiper history kuota

---
+++ src/Mysql/UserDropshiperHistoryKuota.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Kodami\Models\Mysql;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class UserDropshiperHistoryKuota extends Model
+{
+	protected $table = 'user_dropshiper_history_kuota';
+}

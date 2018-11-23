77298a2587665715f09f387a57f71ec39a065e1e
Ramdoni <doni.enginer@gmail.com>
2018-11-23 18:52:38 +0700

    Update

---
+++ src/Mysql/PlnPaketTokenLanggananStrum.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Kodami\Models\Mysql;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PlnPaketTokenLanggananStrum extends Model
+{
+    protected $table = 'pln_paket_token_langganan_strum';
+}

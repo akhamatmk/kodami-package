65f8304754c71a958645ad56be6e2f41fedc5da1
Ramdoni <doni.enginer@gmail.com>
2018-11-30 13:33:57 +0700

    Update model

---
+++ src/Mysql/TransaksiPlnPascabayar.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Kodami\Models\Mysql;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TransaksiPlnPascabayar extends Model
+{
+    protected $table = 'transaksi_pln_pascabayar';
+}
